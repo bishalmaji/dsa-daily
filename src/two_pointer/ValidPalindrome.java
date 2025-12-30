16c6439cc63906f47b386ea7330c7e027966ffb0
Bishal Maji <bishalmaji959@gmail.com>
2025-12-30 15:03:30 +0530

    refactor: valid palindrome readability and documentation

--- src/two_pointer/ValidPalindrome.java
+++ src/two_pointer/ValidPalindrome.java
@@ -1,33 +1,19 @@
-// package two_pointer;
-
-// public class ValidPalindrome {
-
-//     public boolean isPalindrome(String s) {
-//         String t = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-//         int l = 0, r = t.length() - 1;
-//         while (l < r) {
-//             if (t.charAt(l) != t.charAt(r)) return false;
-//             l++;
-//             r--;
-//         }
-//         return true;
-//     }
-// }
-
+// Problem: Valid Palindrome
+// Platform: LeetCode
+// Approach: Two pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 package two_pointer;
 
 public class ValidPalindrome {
+
     public boolean isPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
+        String t = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int l = 0, r = t.length() - 1;
         while (l < r) {
-            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
-            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
-            if (Character.toLowerCase(s.charAt(l)) !=
-                Character.toLowerCase(s.charAt(r))) {
-                return false;
-            }
+            if (t.charAt(l) != t.charAt(r)) return false;
             l++;
             r--;
         }
